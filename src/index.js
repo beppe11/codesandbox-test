1ef43ec2cf92d1c35e8e362032a37b1150756f11
beppe11 <78246566+beppe11@users.noreply.github.com>
2021-02-01 09:17:15 +0900

    map

--- src/index.js
+++ src/index.js
@@ -6,16 +6,28 @@
 // console.log(age);
 // console.log(tall);
 
+// const { age: a, name: n, tall: t } = obj;
+// console.log(a);
+// console.log(n);
+// console.log(t);
+
 // let a = 10, b = 20;
 // [a, b] = [b, a];
 // console.log(a);
 // console.log(b);
 
-const fn = (x, y, z) => {
-  console.log(x);
-  console.log(y);
-  console.log(z);
-};
+// const fn = (x, y, z) => {
+//   console.log(x);
+//   console.log(y);
+//   console.log(z);
+// };
 
-const arr = [1, 2, 3];
-fn(...arr);
+// const arr = [1, 2, 3];
+// fn(...arr);
+
+/**
+ * map
+ */
+const nameArr = ["Yamada", "Tanaka", "Iwamoto"];
+const nameArr2 = nameArr.map((name) => `name: ${name}`);
+nameArr2.map((name) => console.log(name));
